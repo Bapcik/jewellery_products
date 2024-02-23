7ab00ca5d55178afc14c666a5525f5cde4745e6b
Diana <sounfair.02@gmail.com>
2024-02-23 19:19:07 +0600

    8 router addition

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: `http://api.valantis.store:40000`,
+  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
 });
