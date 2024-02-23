0ac5f1df3c00c13ff098aac355b17bdbbfc07f78
Diana <sounfair.02@gmail.com>
2024-02-23 19:59:37 +0600

    bug fix

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  withCredentials: true,
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://api.valantis.store:40000",
 });
